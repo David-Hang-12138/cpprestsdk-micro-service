579471262373d1940f272aa8126dc10d6b8cd51e
David-Hang-12138 <david.hang.12138@gmail.com>
2023-02-21 16:51:14 +0200

    added user maanger

--- libs/build_dependencies.sh
+++ libs/build_dependencies.sh
@@ -20,7 +20,7 @@ install_cpprestsdk(){
 	if [[ "$OSTYPE" == "linux-gnu" ]]; then
 		export CXX=g++-4.9
 	fi
-	(cd "$restsdkBuildDir" && cmake ../Release -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTS=OFF -DBUILD_SAMPLES=OFF)
+	(cd "$restsdkBuildDir" && cmake3 ../Release -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTS=OFF -DBUILD_SAMPLES=OFF)
 	(cd "$restsdkBuildDir" && make)
 }
 
